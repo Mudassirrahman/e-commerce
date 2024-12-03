0b13a9a700bad5cb551c2316869c420a428a85b3
mudassirrahman <mudassirrehman1989@gmail.com>
2024-12-03 22:32:48 +0500

    loader added, code formated,

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, IconButton } from '@mui/material'
+import { Box, CircularProgress, Container, IconButton, Typography } from '@mui/material'
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import axios from 'axios'
@@ -6,20 +6,23 @@ import React, { useEffect, useState } from 'react'
 
 function Products() {
   let [products, setProducts] = useState([])
-  let productsData
+  let [loader,setLoader] = useState(false)
   useEffect(() => {
-    productsData = axios.get("https://fakestoreapi.com/products").then((responce) => setProducts(responce.data))
+    setLoader(true)
+    axios.get("https://fakestoreapi.com/products").then((responce) => {setProducts(responce.data);setLoader(false)})
+    
   },[])
   return (
     <Container>
+    {loader === true ? <Box sx={{ textAlign: "center", m: 5 }}><CircularProgress /></Box> : 
       <Box  className="d-flex flex-wrap mt-5 gap-4 justify-content-lg-between justify-content-md-between justify-content-sm-between justify-content-center align-content-center">{products.map((product) => {
         return(
           <Box key={product.id} className="card flex-grow-1 col-lg-3 col-md-3 col-sm-5 col-12 p-3">
           <img max-width="200px" height="300px" className='card-img-top' src={product.image} />
            <Box className="card-body">
-           <h1 className='card-title'>{product.title.length >= 11 ? product.title.slice(0,11).concat("..."): product.title}</h1>
-           <p className='card-text'>{product.description.length >= 90 ? product.description.slice(0,90).concat("..."): product.description}</p>
-           <p>Price: ${product.price}</p>
+           <Typography variant='h4' className='card-title'>{product.title.length >= 11 ? product.title.slice(0,11).concat("..."): product.title}</Typography>
+           <Typography className='card-text'>{product.description.length >= 90 ? product.description.slice(0,90).concat("..."): product.description}</Typography>
+           <Typography>Price: ${product.price}</Typography>
            </Box>
             <Box className="d-flex justify-content-center gap-5">
             <IconButton size="large" color="info">
@@ -31,7 +34,7 @@ function Products() {
             </Box>
           </Box>
         )
-      })}</Box>
+      })}</Box>}
     </Container>
   )
 }
