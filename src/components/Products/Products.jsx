a2d12591103d71a589aa51d47065480f6e786374
mudassirrahman <mudassirrehman1989@gmail.com>
2024-12-03 13:22:34 +0500

    icon added some css improved in products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,4 +1,6 @@
 import { Box, Container } from '@mui/material'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
@@ -10,12 +12,18 @@ function Products() {
   },[])
   return (
     <Container>
-      <Box  className="d-flex flex-wrap mt-5 gap-4 justify-content-between">{products.map((product) => {
+      <Box  className="d-flex flex-wrap mt-5 gap-4 justify-content-lg-between justify-content-md-between justify-content-sm-between justify-content-center align-content-center">{products.map((product) => {
         return(
-          <Box key={product.id} className="card col-3 p-3">
-          <img width="250px" height="270px" className='' src={product.image} />
-            <h1>{product.title.slice(0,15)}</h1>
-            <p>{product.description.slice(0,90)}</p>
+          <Box key={product.id} className="card flex-grow-1 col-lg-3 col-md-3 col-sm-5 col-12 p-3">
+          <img max-width="200px" height="300px" className='card-img-top' src={product.image} />
+           <Box className="card-body">
+           <h1 className='card-title'>{product.title.length >= 15 ? product.title.slice(0,15).concat("..."): product.title}</h1>
+           <p className='card-text'>{product.description.length >= 90 ? product.description.slice(0,90).concat("..."): product.description}</p>
+           </Box>
+            <Box className="d-flex justify-content-center gap-5">
+              <VisibilityIcon className='' />
+              <AddShoppingCartIcon />
+            </Box>
           </Box>
         )
       })}</Box>
